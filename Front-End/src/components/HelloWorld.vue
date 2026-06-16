d9a56d3f5958ddc933a99c68592245730e354539
_robert <65054826+midoon@users.noreply.github.com>
2026-06-16 20:33:13 +0700

    setup frontend

---
+++ Front-End/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold text-gray-800">Hello, World!</h1>
+  </div>
+</template>
